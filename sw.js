af708de0c5aaeb9aea0803117054f4719f91504f
MarkRustenholtz <markrusten@gmail.com>
2025-09-02 16:20:06 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,8 +4,8 @@ const urlsToCache = [
   './index.html',
   './manifest.json',
   './icon-192.png',
-  './icon-512.png'
-  // Ajoute ici tes fichiers CSS/JS si séparés
+  './icon-512.png',
+  'https://unpkg.com/html5-qrcode' // pour fonctionner offline
 ];
 
 self.addEventListener('install', event => {
@@ -21,7 +21,7 @@ self.addEventListener('activate', event => {
     caches.keys().then(cacheNames =>
       Promise.all(
         cacheNames.map(name => {
-          if(name !== CACHE_NAME) return caches.delete(name);
+          if (name !== CACHE_NAME) return caches.delete(name);
         })
       )
     )
@@ -31,7 +31,12 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).catch(() => {
+        if (event.request.mode === "navigate") {
+          return caches.match('./index.html');
+        }
+      });
+    })
   );
 });
